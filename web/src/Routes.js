0d023336b5a4745cfe64fed0114e22a7518c6c41
Franck <franckvienot7@gmail.com>
2023-07-27 23:13:37 +0200

    added all routes which are going to be protected under /admin/**

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -16,28 +16,28 @@ const Routes = () => {
   return (
     <Router>
       <Set wrap={ScaffoldLayout} title="Roles" titleTo="roles" buttonLabel="New Role" buttonTo="newRole">
-        <Route path="/roles/new" page={RoleNewRolePage} name="newRole" />
-        <Route path="/roles/{id:Int}/edit" page={RoleEditRolePage} name="editRole" />
-        <Route path="/roles/{id:Int}" page={RoleRolePage} name="role" />
-        <Route path="/roles" page={RoleRolesPage} name="roles" />
+        <Route path="/admin/roles/new" page={RoleNewRolePage} name="newRole" />
+        <Route path="/admin/roles/{id:Int}/edit" page={RoleEditRolePage} name="editRole" />
+        <Route path="/admin/roles/{id:Int}" page={RoleRolePage} name="role" />
+        <Route path="/admin/roles" page={RoleRolesPage} name="roles" />
       </Set>
       <Set wrap={ScaffoldLayout} title="Equipments" titleTo="equipments" buttonLabel="New Equipment" buttonTo="newEquipment">
-        <Route path="/equipments/new" page={EquipmentNewEquipmentPage} name="newEquipment" />
-        <Route path="/equipments/{id:Int}/edit" page={EquipmentEditEquipmentPage} name="editEquipment" />
-        <Route path="/equipments/{id:Int}" page={EquipmentEquipmentPage} name="equipment" />
-        <Route path="/equipments" page={EquipmentEquipmentsPage} name="equipments" />
+        <Route path="/admin/equipments/new" page={EquipmentNewEquipmentPage} name="newEquipment" />
+        <Route path="/admin/equipments/{id:Int}/edit" page={EquipmentEditEquipmentPage} name="editEquipment" />
+        <Route path="/admin/equipments/{id:Int}" page={EquipmentEquipmentPage} name="equipment" />
+        <Route path="/admin/equipments" page={EquipmentEquipmentsPage} name="equipments" />
       </Set>
       <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
-        <Route path="/users/new" page={UserNewUserPage} name="newUser" />
-        <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
-        <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
-        <Route path="/users" page={UserUsersPage} name="users" />
+        <Route path="/admin/users/new" page={UserNewUserPage} name="newUser" />
+        <Route path="/admin/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
+        <Route path="/admin/users/{id:Int}" page={UserUserPage} name="user" />
+        <Route path="/admin/users" page={UserUsersPage} name="users" />
       </Set>
       <Set wrap={ScaffoldLayout} title="Annonces" titleTo="annonces" buttonLabel="New Annonce" buttonTo="newAnnonce">
-        <Route path="/annonces/new" page={AnnonceNewAnnoncePage} name="newAnnonce" />
-        <Route path="/annonces/{id:Int}/edit" page={AnnonceEditAnnoncePage} name="editAnnonce" />
-        <Route path="/annonces/{id:Int}" page={AnnonceAnnoncePage} name="annonce" />
-        <Route path="/annonces" page={AnnonceAnnoncesPage} name="annonces" />
+        <Route path="/admin/annonces/new" page={AnnonceNewAnnoncePage} name="newAnnonce" />
+        <Route path="/admin/annonces/{id:Int}/edit" page={AnnonceEditAnnoncePage} name="editAnnonce" />
+        <Route path="/admin/annonces/{id:Int}" page={AnnonceAnnoncePage} name="annonce" />
+        <Route path="/admin/annonces" page={AnnonceAnnoncesPage} name="annonces" />
       </Set>
       <Set wrap={GarageLayout}>
         <Route path="/contact" page={ContactPage} name="contact" />
